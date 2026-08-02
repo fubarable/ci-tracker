32845ca8817d29494eb0ee25afea11beb974b582
fubarable <pete_stein@yahoo.com>
2026-08-01 22:43:42 -0400

    Build v1.3 production assets

--- public/build/assets/chevrons-up-down-BNNc_3XW.js
+++ public/build/assets/chevrons-up-down-BNNc_3XW.js
@@ -1,6 +1,1 @@
-import { t as e } from './createLucideIcon-DhBKMkeN.js';
-var t = e(`chevrons-up-down`, [
-    [`path`, { d: `m7 15 5 5 5-5`, key: `1hf1tw` }],
-    [`path`, { d: `m7 9 5-5 5 5`, key: `sgt6xg` }],
-]);
-export { t };
+import{t as e}from"./createLucideIcon-DhBKMkeN.js";var t=e(`chevrons-up-down`,[[`path`,{d:`m7 15 5 5 5-5`,key:`1hf1tw`}],[`path`,{d:`m7 9 5-5 5 5`,key:`sgt6xg`}]]);export{t};
